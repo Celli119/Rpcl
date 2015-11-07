492f21601392315e6dfd3b07f9e329d31ff703d7
Celli119 <francois.girinon@gadz.org>
2015-11-07 15:18:52 +0100

    add normals

--- src/Rexport.cpp
+++ src/Rexport.cpp
@@ -10,7 +10,7 @@ RcppExport SEXP Rexport(SEXP vb_, SEXP normals_ = Rcpp::wrap(0))
 	try{
 		PclIO cloud;
 		cloud.pclRead(vb_, normals_);
-		return PclIO::RpclToR(cloud.getCloud());
+		return PclIO::RpclToR(cloud.getCloud(), cloud.getNormals());
 	}
 	catch (std::exception& e) {
 		::Rf_error( e.what());
